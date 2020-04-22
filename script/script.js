208613f8d6a57f5a1b2c4c66353ce698deb0e845
jalaniz-sys <60115425+jalaniz-sys@users.noreply.github.com>
2020-04-21 20:51:41 -0500

    added icons

--- script/script.js
+++ script/script.js
@@ -1,7 +1,19 @@
 var mapa;
 function initMap()
 {
+var latlang={lat:4.710859,lng:-74.074955};
+var latlang2={lat:6.247514, lng:-75.571676};
 mapa=new google.maps.Map(
 		document.getElementById('mapa'),
-		{center: new google.maps.LatLng(4.665663, -74.077451), zoom:18});
+		{center: new google.maps.LatLng(4.665663, -74.077451), zoom:7.5});
+
+var marker= new google.maps.Marker({position:latlang,map:mapa});
+
+marker= new google.maps.Marker({
+	map:mapa,
+	draggable:true,
+	animation:google.maps.Animation.BOUNCE,
+	position:new google.maps.LatLng(6.219131, -75.566329),
+	icon:'images/house6.jpg'
+});
 }
