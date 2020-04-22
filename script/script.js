ad1574a3c52254060f7ce0da8c83767a94408be7
jalaniz-sys <60115425+jalaniz-sys@users.noreply.github.com>
2020-04-21 19:36:27 -0500

    added lat and lng

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 var mapa;
 function intitMap()
 {
-	mapa=new google.maps.Map(
+mapa=new google.maps.Map(
 		document.getElementById('mapa'),
-		{center: new google.maps.LatLng(0,0), zoom:18});
+		{center: new google.maps.LatLng(4.665663, -74.077451), zoom:18});
 }
